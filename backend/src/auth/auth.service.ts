c6a4f43168e6d7cbcc5f031a266b09dfbb06fff1
Francisco Gemaque <gemaquejr@hotmail.com>
2025-01-12 12:00:43 -0300

    Implementa o Passport e JWT no auth.service.ts
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private prisma: PrismaService,
+  ) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { ...result } = user;
+      return result;
+    }
+    throw new UnauthorizedException('Email ou senha inválidos');
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id, role: user.role };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
