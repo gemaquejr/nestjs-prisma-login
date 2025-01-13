e4e17142b1342e0061436752021be133322e0e1a
Francisco Gemaque <gemaquejr@hotmail.com>
2025-01-12 22:43:10 -0300

    Adiciona testes para prisma.service.ts
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- backend/src/prisma/prisma.service.spec.ts
+++ backend/src/prisma/prisma.service.spec.ts
@@ -10,9 +10,29 @@ describe('PrismaService', () => {
     }).compile();
 
     service = module.get<PrismaService>(PrismaService);
+    jest.spyOn(service, '$connect').mockResolvedValue();
+    jest.spyOn(service, '$disconnect').mockResolvedValue();
+  });
+
+  afterEach(async () => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('onModuleInit', () => {
+    it('should call $connect when module is initialized', async () => {
+      await service.onModuleInit();
+      expect(service.$connect).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('onModuleDestroy', () => {
+    it('should call $disconnect when module is destroyed', async () => {
+      await service.onModuleDestroy();
+      expect(service.$disconnect).toHaveBeenCalledTimes(1);
+    });
   });
 });
