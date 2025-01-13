ed3bf268ccf1cd3a14b31be7d228910210ccedff
Francisco Gemaque <gemaquejr@hotmail.com>
2025-01-12 21:53:22 -0300

    Adiciona testes para o módulo user
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            createUser: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call findAll method', async () => {
+    await controller.findAll();
+    expect(service.findAll).toHaveBeenCalled();
+  });
+
+  it('should call findOne method', async () => {
+    const id = 1;
+    await controller.findOne(id.toString());
+    expect(service.findOne).toHaveBeenCalledWith(id);
   });
 });
--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { Role } from './role.enum';
 
 describe('UserService', () => {
   let service: UserService;
+  let prismaService: PrismaService;
+
+  const mockPrismaService = {
+    user: {
+      findMany: jest.fn().mockResolvedValue([{ id: 1, email: 'user@example.com', role: 'USER' }]), // Mock do findMany
+      create: jest.fn().mockResolvedValue({ id: 1, email: 'user@example.com', role: 'USER' }),
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        { provide: PrismaService, useValue: mockPrismaService },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('findAll should return an array of users', async () => {
+      const users = await service.findAll();
+      expect(users).toEqual([{ id: 1, email: 'user@example.com', role: 'USER' }]);
+    });
   });
 });
