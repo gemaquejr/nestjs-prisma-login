7c7bae1ec9dac3008a7761f7fab1003af9c16670
Francisco Gemaque <gemaquejr@hotmail.com>
2025-01-12 22:42:05 -0300

    Adiciona testes para user.controller.ts pra função createUser
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -14,8 +14,12 @@ describe('UserController', () => {
           provide: UserService,
           useValue: {
             findAll: jest.fn(),
-            findOne: jest.fn(),
-            createUser: jest.fn(),
+            findOne: jest.fn().mockResolvedValue({ id: 1, email: 'test@test.com' }),
+            createUser: jest.fn().mockResolvedValue({
+              id: 1,
+              email: 'test@test.com',
+              role: 'USER',
+            }),
           },
         },
       ],
@@ -38,5 +42,23 @@ describe('UserController', () => {
     const id = 1;
     await controller.findOne(id.toString());
     expect(service.findOne).toHaveBeenCalledWith(id);
+  });
+
+  describe('createUser', () => {
+    it('should call createUser method of UserService with correct parameters', async () => {
+      const body = { email: 'test@test.com', password: '123456', role: 'USER' };
+      await controller.createUser(body);
+      expect(service.createUser).toHaveBeenCalledWith(body.email, body.password);
+    });
+
+    it('should return the created user object', async () => {
+      const body = { email: 'test@test.com', password: '123456', role: 'USER' };
+      const result = await controller.createUser(body);
+      expect(result).toEqual({
+        id: 1,
+        email: 'test@test.com',
+        role: 'USER',
+      });
+    });
   });
 });
